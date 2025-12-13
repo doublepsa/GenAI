66fc4cc66b36a9c566dc0c4e4f3a4107d4fb6871
BEA1323 <burakerelakgun@hotmail.com>
2025-12-13 16:52:58 +0100

    Uploaded db enviroment file

--- db_configs/db_connection.py
+++ db_configs/db_connection.py
@@ -1,16 +1,20 @@
 from mongoengine import connect
+from dotenv import load_dotenv
+import os
+# Load the config file
+load_dotenv()
 
 class MongoDBConnection:
     @classmethod
-    def setup(cls, db_name="GenAi_WS25", host="localhost", port=27017):
+    def setup(cls):
+        db_name = os.getenv("MONGO_DB_NAME", "GenAi_WS25")
+        host = os.getenv("MONGO_HOST", "localhost")
+        port = int(os.getenv("MONGO_PORT", 27017))
+
         try:
-            connect(
-                db=db_name,
-                host=host, 
-                port=port
-            )
-            print(f"Connection successful to DB: '{db_name}' at {host}:{port}")
+            connect(db=db_name, host=host, port=port)
+            print(f"Connected to local DB: {db_name}")
             return True
         except Exception as e:
-            print(f"Connection Failed! Error: {e}")
+            print(f"Connection Failed: {e}")
             return False
